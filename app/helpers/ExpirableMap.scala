7242dfa2af771f81bc92c7fac5825bd516733bc2
andu125 <andu125@o2.pl>
2017-03-27 10:39:08 +0200

    ExpirableMap bug fixes.

--- app/helpers/ExpirableMap.scala
+++ app/helpers/ExpirableMap.scala
@@ -2,16 +2,16 @@ package helpers
 
 import org.joda.time.DateTime
 
-import scala.util.{Failure, Success}
+import scalaz.Scalaz._
+import scala.util.{Failure, Success, Try}
 import scala.concurrent.{ExecutionContext, Future}
-
+import scala.concurrent.duration._
+import scala.concurrent.duration.Duration.Zero
 import rx.lang.scala.Observable
 import rx.lang.scala.subjects.PublishSubject
 import rx.lang.scala.schedulers.ExecutionContextScheduler
-
-import com.github.nscala_time.time.Implicits.{richReadableInstant,richDuration}
-
-import helpers.ConcurrentMap.SimpleMap
+import com.github.nscala_time.time.Implicits.richReadableInstant
+import helpers.ConcurrentMap._
 
 object ExpirableMap {
   def apply[Id,Val](aMap: SimpleMap[Id,(DateTime,Val)])(implicit aContext: ExecutionContext) =
@@ -21,32 +21,44 @@ object ExpirableMap {
 class ExpirableMap[Id,Val](
   aMap    : SimpleMap[Id,(DateTime,Val)],
   aContext: ExecutionContext
-) extends ConcurrentMap[Id,(DateTime,Val)](aMap, aContext) {
+) extends ConcurrentMap[Id,(DateTime,Val)](aMap, aContext) with AutoCloseable {
 
-  protected val mMaxTime    = new DateTime(Long.MaxValue)
-  protected val mScheduler  = ExecutionContextScheduler(aContext)
-  protected val mPublisher  = PublishSubject[DateTime]()
-  protected val mSubscriber =
-    mPublisher.scan[(DateTime,DateTime)]((mMaxTime,mMaxTime)){
-      case ((x,y),z) if z < y => (y,z)
-      case ((_,x),_)          => (x,x)
-    }.filter{case(x,y)=> x > y}
+  protected val mMaxTime      = new DateTime(Long.MaxValue)
+  protected val mSubject      = PublishSubject[DateTime]()
+  protected val mScheduler    = ExecutionContextScheduler(aContext)
+  protected val mSubscription =
+    mSubject.scan((true,mMaxTime)){
+      case ((_,y),z) if z < y => (true,z)
+      case ((_,x),_)          => (false,x)
+    }.filter{_._1}
      .map{_._2}
      .switchMap{x=>
         Observable
-          .timer{(DateTime.now to x).toDuration.toScalaDuration}
+          .timer{toDuration(x)}
           .map{_=>x}
     }.subscribeOn(mScheduler)
      .observeOn(mScheduler)
      .subscribe{x=>
         aMap.filterNot{_._2._1 > x}
-            .foreach{x => aMap.remove{x._1}}
+            .foldLeft(().point[Result]){
+              case (acc,(id,_))=> for {
+                _<-acc
+                _<-remove(id)
+              } yield ()
+            } |> run
       }
+
+  def toDuration(aTime: DateTime) = {
+    if(DateTime.now >= aTime) Zero
+    else Try{(aTime.getMillis - DateTime.now.getMillis) millis}.getOrElse(365 days)
+  }
 
   override def run[T](aAction: Result[T]): Future[T] = super.run(aAction).andThen{
     case f@Failure(_) => f
     case s@Success(_) =>
-      if(aMap.nonEmpty) mPublisher onNext {aMap.map{_._2._1}.minBy(_.millis)}
+      if(aMap.nonEmpty) mSubject onNext {aMap.map{_._2._1}.minBy(_.millis)}
       s
   } (aContext)
+
+  override def close(): Unit = mSubscription.unsubscribe()
 }
